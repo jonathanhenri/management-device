5c09336b56266f80cbb6de057ee20c8fd29bdfb5
jonathan henrique de melo souza <jonathan.melosouza@gmail.com>
2024-12-23 15:01:22 -0300

    create api

--- src/main/java/com/global/device/app/api/DeviceController.java
+++ src/main/java/com/global/device/app/api/DeviceController.java
@@ -24,4 +24,62 @@ public class DeviceController {
 		DeviceRecord createdDevice = deviceService.createDevice(deviceRecord);
 		return new ResponseEntity<>(createdDevice, HttpStatus.CREATED);
 	}
+//
+//	// 2. Get device by identifier
+//	@GetMapping("/{identifier}")
+//	public ResponseEntity<Device> getDeviceByIdentifier(@PathVariable String identifier) {
+//		Device device = deviceProvider.getDeviceByIdentifier(identifier);
+//		if (device != null) {
+//			return new ResponseEntity<>(device, HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+//
+//	// 3. List all devices
+//	@GetMapping
+//	public ResponseEntity<List<Device>> listAllDevices() {
+//		List<Device> devices = deviceProvider.listAllDevices();
+//		return new ResponseEntity<>(devices, HttpStatus.OK);
+//	}
+//
+//	// 4. Update device (full update)
+//	@PutMapping("/{identifier}")
+//	public ResponseEntity<Device> updateDevice(@PathVariable String identifier, @RequestBody Device device) {
+//		Device updatedDevice = deviceProvider.updateDevice(identifier, device);
+//		if (updatedDevice != null) {
+//			return new ResponseEntity<>(updatedDevice, HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+//
+//	// 4. Partial update device
+//	@PatchMapping("/{identifier}")
+//	public ResponseEntity<Device> partialUpdateDevice(@PathVariable String identifier, @RequestBody Device device) {
+//		Device updatedDevice = deviceProvider.partialUpdateDevice(identifier, device);
+//		if (updatedDevice != null) {
+//			return new ResponseEntity<>(updatedDevice, HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+//
+//	// 5. Delete a device
+//	@DeleteMapping("/{identifier}")
+//	public ResponseEntity<Void> deleteDevice(@PathVariable String identifier) {
+//		boolean isDeleted = deviceProvider.deleteDevice(identifier);
+//		if (isDeleted) {
+//			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//		} else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+//
+//	// 6. Search device by brand
+//	@GetMapping("/search")
+//	public ResponseEntity<List<Device>> searchDeviceByBrand(@RequestParam String brand) {
+//		List<Device> devices = deviceProvider.searchDeviceByBrand(brand);
+//		return new ResponseEntity<>(devices, HttpStatus.OK);
+//	}
 }
--- src/main/java/com/global/device/infra/dataProvider/DeviceProviderImpl.java
+++ src/main/java/com/global/device/infra/dataProvider/DeviceProviderImpl.java
@@ -4,6 +4,7 @@ import com.global.device.domain.DeviceProvider;
 import com.global.device.domain.entity.Device;
 import com.global.device.infra.mapper.DeviceDataMapper;
 import com.global.device.infra.repository.DeviceRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ public class DeviceProviderImpl implements DeviceProvider {
 	private DeviceDataMapper deviceDataMapper;
 	
 	@Override
+	@Transactional
 	public Device createDevice(Device device) {
 		return deviceDataMapper.toData(deviceRepository.save(deviceDataMapper.toEntity(device)));
 	}
